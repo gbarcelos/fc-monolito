b211f5436407d0bc34ab42ca4bfbca7bdc684497
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-03-12 13:54:15 -0300

    Adicona o pacote factory

---
+++ src/modules/invoice/factory/invoice.facade.factory.ts
@@ -0,0 +1,18 @@
+import InvoiceFacade from "../facade/invoice.facade";
+import { InvoiceFacadeInterface } from "../facade/invoice.facade.interface";
+import InvoiceRepository from "../repository/invoice.repository";
+import FindInvoiceUseCase from "../usecase/find/find.usecase";
+import GenerateInvoiceUseCase from "../usecase/generate/generate.usecase";
+
+export default class InvoiceFacadeFactory {
+
+  static create(): InvoiceFacadeInterface {
+
+    const repository = new InvoiceRepository();
+
+    return new InvoiceFacade({
+      generateInvoiceUseCase: new GenerateInvoiceUseCase(repository),
+      findInvoiceUseCase: new FindInvoiceUseCase(repository)
+    });
+  }
+}
